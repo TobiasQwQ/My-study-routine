8d8b8b27e4e8315a0021288c513de25166234838
QwQ <113231969+TobiasQwQ@users.noreply.github.com>
2022-09-11 14:28:32 +0800

    Add files via upload

---
+++ Untitled-5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 96;
+    if(age<18)
+    printf("未成年\n");
+    else
+    printf("成年\n");
+    return 0;
+}
---
+++ Untitled-6.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int A=1;
+    int B=2;
+    if (A == 1)
+    {
+        if(B == 2)
+        printf("HEHE\n");
+    }
+    else
+    printf("HAHA\n");
+    return 0;
+}
